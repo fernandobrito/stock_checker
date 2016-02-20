739a23185e33b023acb9b02158455ffe66e84da3
Fernando Brito <email@fernandobrito.com>
2016-02-20 00:40:52 -0300

    Add colors to product readded notification

--- lib/stock_checker/comparators/product_comparator.rb
+++ lib/stock_checker/comparators/product_comparator.rb
@@ -3,6 +3,8 @@ module StockChecker
   # Compare two products. Generate alerts if the product did not exist but now it does,
   #  or vice-versa.
   class ProductComparator < Comparator
+    PRIORITY = 30
+
     def compare(old, new)
       # It existed, and it still does
       if (old.exists?) && (new.exists?)
@@ -15,14 +17,14 @@ module StockChecker
         Logging::logger.info '== [ProductComparator] Product did not exist, but now it does'
 
         @continue = false
-        return [ Notification.new(new, 'Product readded') ]
+        return [ Notification.new(new, "<span class='green strong'>Product readded</span>", PRIORITY) ]
 
       # It existed, but now it does not
       elsif (old.exists?) && (! new.exists?)
         Logging::logger.info '== [ProductComparator] Product existed, but now it does not'
 
         @continue = false
-        return [ Notification.new(old, 'Product removed') ]
+        return [ Notification.new(old, "<span class='red strong'>Product removed</span>", PRIORITY) ]
 
       # Product did not exist, and still does not
       elsif (! old.exists?) && (! new.exists?)
